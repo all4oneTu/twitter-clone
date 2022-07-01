076c76b462c5f65fa2c75f498598a9e96713b67c
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2022-07-01 10:06:18 +0700

    update

--- pages/api/addTweet.ts
+++ pages/api/addTweet.ts
@@ -1,4 +1,3 @@
-import { mutations } from './../../.history/typings.d_20220630225735';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { TweetBody } from '../../typings'
